f114dbb501fc0828fcb5b218ffd9363f0a35fab0
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-05-06 00:01:01 +0530

    add vercel link in origin

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
+  "https://mern-ecommerce-frontend-hazel.vercel.app",
   "https://mern-stylenest.onrender.com",
 ];
 
